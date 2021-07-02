be35174295aaac444f3e6afb0bcc1dca5a716362
Aakash Jangid <ajangid25@gmail.com>
2021-07-02 16:00:57 +0530

    react-router-dom and @types-react-router-dom added, project configured with mui

---
+++ src/Components/SideNav/SideNav.tsx
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({});
+
+const SideNav = () => {
+  const classes = useStyles();
+  return <div>SideNav</div>;
+};
+
+export default SideNav;
